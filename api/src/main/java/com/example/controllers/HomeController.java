5e352a34eccbc4a733737173599ad9c21a2475f1
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-06-04 12:51:17 +0300

    initial 1

--- api/src/main/java/com/example/controllers/HomeController.java
+++ api/src/main/java/com/example/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.api.controllers;
+package com.example.controllers;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
